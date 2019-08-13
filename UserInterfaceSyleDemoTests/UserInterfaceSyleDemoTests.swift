344ba468e69afc97e0e918a268ff044a6ea1c809
Kent Nagata <ngtknt@me.com>
2019-08-13 17:06:23 +0900

    Initial Commit

---
+++ UserInterfaceSyleDemoTests/UserInterfaceSyleDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  UserInterfaceSyleDemoTests.swift
+//  UserInterfaceSyleDemoTests
+//
+//  Created by Kento Nagata on 2019/08/13.
+//  Copyright © 2019 ngtk. All rights reserved.
+//
+
+import XCTest
+@testable import UserInterfaceSyleDemo
+
+class UserInterfaceSyleDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
